5795507481e81b386fc464b9a7b584f3a3877a4e
89Aman <shasarita23@gmail.com>
2026-05-13 23:42:02 +0530

    docs: add JSDoc for Firebase initialization

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,6 @@
+/**
+ * Firebase Admin SDK instance.
+ */
 const admin = require('firebase-admin');
 
 if (!admin.apps.length) {
@@ -7,7 +10,15 @@ if (!admin.apps.length) {
 }
 
 const { getFirestore } = require('firebase-admin/firestore');
+
+/**
+ * Firestore database instance.
+ */
 const db = getFirestore(admin.app(), 'default');
+
+/**
+ * Firebase Auth instance.
+ */
 const auth = admin.auth();
 
 module.exports = { admin, db, auth };
